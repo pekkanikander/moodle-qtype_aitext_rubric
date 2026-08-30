1a1ccade37e52ed0970aaa363e5147b3ab9c3e7b
Marcus Green <marcusavgreen@gmail.com>
2026-08-30 18:57:43 +0100

    Loosen behaviour dependency pins to ANY_VERSION
    Change qbehaviour_deferred_for_aitext and
    qbehaviour_immediate_for_aitext dependencies from an exact
    version pin to ANY_VERSION so the behaviours can be bumped
    without requiring a matching pin edit here.

--- version.php
+++ version.php
@@ -31,6 +31,6 @@ $plugin->release = '2.1.0';
 $plugin->supported = [405, 502];
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
-    'qbehaviour_deferred_for_aitext' => 2026050500,
-    'qbehaviour_immediate_for_aitext' => 2026050500,
+    'qbehaviour_deferred_for_aitext' => ANY_VERSION,
+    'qbehaviour_immediate_for_aitext' => ANY_VERSION,
 ];
